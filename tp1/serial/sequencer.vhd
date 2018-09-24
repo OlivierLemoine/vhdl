b9187d7365b490619b14657db5642cfa45eb4da0
OlivierLemoine <16369060+OlivierLemoine@users.noreply.github.com>
2018-09-24 18:39:16 +0200

    Add files via upload

--- tp1/serial/sequencer.vhd
+++ tp1/serial/sequencer.vhd
@@ -12,40 +12,38 @@ end sequencer;
 
 architecture arch_sequencer of sequencer is
 
-    type inner_state is (idle, loading, counting);
+    type inner_state is (resetting, idle, loading, counting);
     signal current_state, next_state : inner_state;
-    signal is_tmp_used : std_logic;
 
 begin
     sync : process( clk, reset )
     begin
         if reset='1' then
-            raz_count <= '1';
-            raz_ser <= '1';
-            raz_st <= '1';
-            current_state <= idle;
+            current_state <= resetting;
         elsif rising_edge(clk) then
-            if comptage="1010" then
-                raz_count <= '1';
-            else
-                raz_count <= '0';
-            end if ;
-            raz_ser <= '0';
-            raz_st <= '0';
             current_state <= next_state;
         end if ;
     end process ; -- sync
 
-    combination : process( current_state, start, comptage )
+    combination : process( current_state, start, comptage, ld_t )
     begin
         ce <= '0';
         ser <= '0';
         ld_ser <= '0';
+        raz_st <= '0';
+        raz_ser <= '0';
+        raz_count <= '0';
         next_state <= current_state;
         case current_state is
+            when resetting =>
+                raz_st <= '1';
+                raz_ser <= '1';
+                raz_count <= '1';
+                if ld_t='1' then
+                    next_state <= idle;
+                end if ;
             when idle =>
-                if start='1'
-                then
+                if start='1' then
                     next_state <= loading;
                     ld_ser <= '1';
                 end if;
@@ -53,16 +51,13 @@ begin
                 next_state <= counting;
                 ce <= '1';
                 ser <= '1';
-                when counting =>
+                set_st <= '1';
+            when counting =>
                 ce <= '1';
                 ser <= '1';
-                if comptage="1010" then
-                    next_state <= idle;
+                if comptage="1001" then
+                    next_state <= resetting;
                 end if ;
         end case;
     end process ; -- combination
-
-    is_tmp_used <= ((ld_t or is_tmp_used) and not start) and not reset;
-    set_st <= is_tmp_used;
-    raz_st <= not is_tmp_used or reset;
 end arch_sequencer;
